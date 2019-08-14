71fb66c32a9392ddcae15e10e15793a7caea277f
Isacc Avitia <iavitia@outlook.com>
2019-08-14 00:34:45 -0700

    Created user model & registration

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
 
 const app = express();
+
+// bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Database config
 const db = require("./config/keys").mongoURI;
@@ -12,9 +19,8 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("", (req, res) => {
-  res.send("Home page");
-});
+// User routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 

184e26d75e49cd22fa04ebe856f1989a9d79630f
Isaac <iavitia@outlook.com>
2019-09-12 09:23:32 -0700

    Fixed footer position

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="bg-white text-center">
+    <footer className="footer">
       Copyright &copy; {new Date().getFullYear()} Timelined
     </footer>
   );

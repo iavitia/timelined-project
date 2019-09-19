d8a28246926a6f4a6edd1114995d07933a35d28c
Isaac <iavitia@outlook.com>
2019-09-18 23:16:42 -0700

    register-diplay errors if email or username exist

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,29 +20,49 @@ router.post("/register", (req, res) => {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      errors.email = "Email already exists";
-      return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      });
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
+  User.findOne({
+    $or: [
+      {
+        email: req.body.email
+      },
+      {
+        username: req.body.username
+      }
+    ]
+  })
+    .then(user => {
+      if (user) {
+        if (user.username === req.body.username) {
+          errors.username = "Username already exists";
+        }
+        if (user.email === req.body.email) {
+          errors.email = "Email already exists";
+        }
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password
         });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({
+        error: err
       });
-    }
-  });
+    });
 });
 
 // POST api/users/login - return JWT Token

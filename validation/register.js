54c12363537cbcd7a6dc7f28341c5d9d80c949ae
Isaac <iavitia@outlook.com>
2019-09-19 19:58:05 -0700

    improved password validation

--- validation/register.js
+++ validation/register.js
@@ -1,10 +1,27 @@
 const validator = require("validator");
+const passwordValidator = require("password-validator");
 const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
+  const passwordSchema = new passwordValidator();
   const whiteListed =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
   let errors = {};
+
+  passwordSchema
+    .is()
+    .min(8)
+    .is()
+    .max(30)
+    .has()
+    .uppercase()
+    .has()
+    .lowercase()
+    .has()
+    .digits()
+    .has()
+    .not()
+    .spaces();
 
   // Makes data an empty string if it's empty before using validator
   data.username = !isEmpty(data.username) ? data.username : "";
@@ -12,12 +29,12 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
 
   // Username validation
-  if (validator.isEmpty(data.username)) {
-    errors.username = "Username is required";
-  }
-
   if (!validator.isLength(data.username, { min: 3, max: 20 })) {
     errors.username = "Username must be between 3 and 20 characters";
+  }
+
+  if (validator.isEmpty(data.username)) {
+    errors.username = "Username is required";
   }
 
   if (!validator.isWhitelisted(data.username, whiteListed)) {
@@ -26,24 +43,22 @@ module.exports = function validateRegisterInput(data) {
   }
 
   // Email validation
-  if (validator.isEmpty(data.email)) {
-    errors.email = "Email is required";
-  }
-
   if (!validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
 
-  // Password validation
-  if (validator.isEmpty(data.password)) {
-    errors.password = "Password is required";
-  }
-  if (!validator.isLength(data.password, { min: 8, max: 30 })) {
-    errors.password = "Password must be between 8 and 30 characters";
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
   }
 
-  if (validator.contains(data.password, " ")) {
-    errors.password = "Password cannot contain spaces";
+  // Password validation
+  if (!passwordSchema.validate(data.password)) {
+    errors.password =
+      "Password must contain upper & lowercase letters, numbers, special characters, and be between 8 & 30 characters";
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = "Password is required";
   }
 
   return {
